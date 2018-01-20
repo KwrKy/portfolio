dc3e7f500b04a907187b0cbfcc2bfc74b4c4fa25
KwrKy <david@creatord.co.za>
2018-01-20 18:54:45 +0200

    Removed home.js popout functionalit at 768 breakpoint

--- js/home.js
+++ js/home.js
@@ -48,76 +48,83 @@ function moveNameCreate() {
 }
 
 //music panels popout
-$(function () {
-  $("#list-music").click(function () {
-    $("#media-container").css("zIndex", "85");
-    $('.audio-player-wrapper-home').addClass('open');
-    $('.video-player-wrapper').addClass('open');
-    $('#close-media').addClass('open');
-    moveNameMusic();
-    toggleScroll();
-  });
+if (document.documentElement.clientWidth > 767) {
+
   $(function () {
-    $("#close-media").click(function () {
-      $("#media-container").css("zIndex", "0");
-      $('.audio-player-wrapper-home').removeClass('open');
-      $('.video-player-wrapper').removeClass('open');
-      $('#close-media').removeClass('open');
-      resetLeft();
+    $("#list-music").click(function () {
+      $("#media-container").css("zIndex", "85");
+      $('.audio-player-wrapper-home').addClass('open');
+      $('.video-player-wrapper').addClass('open');
+      $('#close-media').addClass('open');
+      moveNameMusic();
       toggleScroll();
     });
+    $(function () {
+      $("#close-media").click(function () {
+        $("#media-container").css("zIndex", "0");
+        $('.audio-player-wrapper-home').removeClass('open');
+        $('.video-player-wrapper').removeClass('open');
+        $('#close-media').removeClass('open');
+        resetLeft();
+        toggleScroll();
+      });
+    });
   });
-});
+}
 
 //code panel popout
-$(function () {
-  $("#list-code").click(function () {
-    $('#source-code').addClass('open');
-    $('#close-source').addClass('open');
-    $("<pre />", {
-      "html": '&lt;!DOCTYPE html>\n&lt;html>\n' +
-        $("html")
-        .html()
-        .replace(/[<>]/g, function (m) {
-          return {
-            '<': '&lt;',
-            '>': '&gt;'
-          }[m]
-        })
-        .replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi, '<a href="$1">$1</a>') +
-        '\n&lt;/html>',
-      "class": "prettyprint"
-    }).appendTo("#source-code");
-    prettyPrint();
-    moveNameCode();
-    toggleScroll();
-  });
+if (document.documentElement.clientWidth > 767) {
   $(function () {
-    $("#close-source").click(function () {
-      $('#source-code').removeClass('open');
-      $('#close-source').removeClass('open');
-      resetLeft();
+    $("#list-code").click(function () {
+      $('#source-code').addClass('open');
+      $('#close-source').addClass('open');
+      $("<pre />", {
+        "html": '&lt;!DOCTYPE html>\n&lt;html>\n' +
+          $("html")
+          .html()
+          .replace(/[<>]/g, function (m) {
+            return {
+              '<': '&lt;',
+              '>': '&gt;'
+            }[m]
+          })
+          .replace(/((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi, '<a href="$1">$1</a>') +
+          '\n&lt;/html>',
+        "class": "prettyprint"
+      }).appendTo("#source-code");
+      prettyPrint();
+      moveNameCode();
       toggleScroll();
     });
+    $(function () {
+      $("#close-source").click(function () {
+        $('#source-code').removeClass('open');
+        $('#close-source').removeClass('open');
+        resetLeft();
+        toggleScroll();
+      });
+    });
   });
-});
+}
 
 //create panel popout
-$(function () {
-  $("#list-create").click(function () {
-    $("#create-container").css("zIndex", "0");
-    $('.create-content-container').addClass('open');
-    $('#close-create').addClass('open');
-    moveNameCreate();
-    toggleScroll();
-  });
+if (document.documentElement.clientWidth > 767) {
   $(function () {
-    $("#close-create").click(function () {
-      $("#create-container").css("zIndex", "-100");
-      $('.create-content-container').removeClass('open');
-      $('#close-create').removeClass('open');
-      resetLeft();
+    $("#list-create").click(function () {
+      $("#create-container").css("zIndex", "0");
+      $('.create-content-container').addClass('open');
+      $('#close-create').addClass('open');
+      moveNameCreate();
       toggleScroll();
     });
+    $(function () {
+      $("#close-create").click(function () {
+        $("#create-container").css("zIndex", "-100");
+        $('.create-content-container').removeClass('open');
+        $('#close-create').removeClass('open');
+        resetLeft();
+        toggleScroll();
+      });
+    });
   });
-});
+}
